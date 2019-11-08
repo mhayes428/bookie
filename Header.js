3f22fdf5e5ac0bdbfa0547a2f78011f6a572ce03
mhayes428 <fikes428@gmail.com>
2019-11-08 12:36:43 -0500

    add book icon -- header.js sheet

---
+++ Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <i class="fas fa-book-open"></i>
+            <h1>Bookee</h1>
+        </header>
+    )
+}
