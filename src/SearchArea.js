d3f3141e3300d30b50fdba03b6f74ac1f01b8a8c
mhayes428 <fikes428@gmail.com>
2019-11-08 13:13:15 -0500

    searchArea.js iimport

---
+++ src/SearchArea.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { arrayExpression } from '@babel/types';
+
+const SearchArea = () => {
+    return (
+        <div className="search-area">
+            <form action="">
+                <input type="text"/>
+            </form>
+        </div>
+    )
+}
+
+export default SearchArea;
