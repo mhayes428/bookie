3de098604585b26ac8254a2db74a24fb0960e885
mhayes428 <fikes428@gmail.com>
2019-11-08 22:59:34 -0500

    bookCard Layout

--- src/BookCard.js
+++ src/BookCard.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const BookCard = (props) => {
     return (
-        <div>
+        <div className="card-container">
+            <img src="" alt="" />
+            <div className="desc">
+                <h2></h2>
+                <h3></h3>
+                <p></p>
+            </div>
         </div>
     )
 }
