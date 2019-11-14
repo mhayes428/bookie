14e49f7c25a924ee67c7ba5774fa01096d1f2d23
mhayes428 <fikes428@gmail.com>
2019-11-13 19:08:38 -0500

    added animate css---recolored header

--- src/BookCard.js
+++ src/BookCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BookCard = (props) => {
     return (
-        <div className="card-container">
+        <div className="card-container animated rollIn">
             <img src={props.image} alt=""/>
             <div className="desc">
                 <h2>{props.title}</h2>
